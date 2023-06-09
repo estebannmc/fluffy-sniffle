8ac553b9fd8809bd9f66fa72f8c848a3eb8bcd20
EstebannC <estebanmcordoba@gmail.com>
2023-06-09 09:34:55 -0300

    error2 pixiJS7

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,29 @@ const app = new Application({
 	height: 480
 });
 
+window.addEventListener("resize", ()=>{
+	console.log("Escalado!");
+	const escalaX = window.innerWidth / app.screen.width;
+	const escalaY = window.innerHeight / app.screen.height;
+	const escala = Math.min(escalaX , escalaY);
+	
+	const gameHeight = app.screen.height * escala;
+	const gameWidht = app.screen.width * escala;
+
+	const marginHoriz = (window.innerWidth - gameWidht) / 2;
+	const marginVert = (window.innerHeight - gameHeight) / 2; 
+	
+	app.view.style.width = gameWidht + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHoriz + "px";
+	app.view.style.marginLeft = marginVert + "px";
+
+	app.view.style.marginTop = marginVert + "px";
+	app.view.style.marginBottom = marginVert + "px";
+
+})
+
 Assets.add("sticker", "./animoji.png");
 Assets.add("clampy", "./clampy.png");
 
